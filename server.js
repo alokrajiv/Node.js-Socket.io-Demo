b503ad65f5e2c0984f6e9de8d09e37ba4aac6546
alokrajiv <mail@alokrajiv.com>
2016-04-06 11:27:01 +0400

    Feature add

--- server.js
+++ server.js
@@ -79,6 +79,9 @@ app.post('/missions', function(req, res) {
 });
 
 io.on('connection', function(socket) {
+    socket.on('requestDataMissions', function(data) {
+        socket.send({ data: missions });
+    });
     socket.on('channel1', function(data) {
         socket.broadcast.emit('channel1', data);
     });
